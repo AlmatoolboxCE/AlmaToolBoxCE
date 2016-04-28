37f1ddfe959a76c1948fbd1b1f0cffb18af55208
Bertolini Laura <l.bertolini@almaviva.it>
2016-04-28 16:04:45 +0200

    aggiunti parametri di connessione, ancora non si riesce ad intercettare l'output per capire dove va in errore

--- Openshift_TFS_Extensions/oc3buildTask/oc.ps1
+++ Openshift_TFS_Extensions/oc3buildTask/oc.ps1
@@ -1,19 +1,38 @@
 param (
 	[string]$ocExe,
+	[string]$server,
+	[string]$username,
+	[string]$password,
+	[string]$clientCert,
+	[bool]$skipTls,
+	[string]$buildconfig,
     [string]$token,
     [string]$namespace
 )
 
-Write-Verbose 'Entering oc.ps1'
-Write-Verbose "ocExe = $ocExe"
-Write-Verbose "token = $token"
-Write-Verbose "namespace = $namespace"
+
+"Entering oc.ps1"
+
+"ocExe = $ocExe"
+"server = $server"
+"buildconfig = $buildconfig"
+"token = $token"
+"namespace = $namespace"
+"skipTls = $skipTls"
+"clientCert empty? !$clientCert"
+"username empty? !$username"
+"password empty? !$password"
 
 # Import the Task.Common dll that has all the cmdlets we need for Build
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
 if (-not (Test-Path -Path $ocExe -PathType Leaf)) {
     throw 'Openshift client not installed in the provided location.'
+}
+
+if(!$buildconfig)
+{
+    throw (Get-LocalizedString -Key "Buildconfig parameter is not set")
 }
 
 if(!$token)
@@ -26,19 +45,35 @@ if(!$namespace)
     throw (Get-LocalizedString -Key "Namespace parameter is not set")
 }
 
+if(!$clientCert -end (!$username -or !$password))
+{
+    throw (Get-LocalizedString -Key "Either clientCert or username and password must be set")
+}
+
 if(!(Test-Path $token -PathType Leaf))
 {
     throw ("$token does not exist");
 }
-Write-Verbose "Reading token content"
-$tokenContent = [IO.File]::ReadAllText("$token")
 
-Write-Verbose "Calling Openshift client"
+"Reading token content"
+$tokenContent = [IO.File]::ReadAllText("$token").Trim()
 
-& $ocExe --token="$tokenContent" -n $namespace 2>&1 
+"Constructing login parameters"
+$loginCredentials = If (!$clientCert) {
+	-u $username -p $password
+} Else {
+	--certificate-authority="$clientCert"
+} 
+
+"Logging in: $ocExe login $server $loginCredentials --insecure-skip-tls-verify=$skipTls"
+& $ocExe login $server $loginCredentials --insecure-skip-tls-verify=$skipTls 2>&1
+
+"Calling start-build $ocExe --token="$tokenContent" start-build $buildconfig --follow -n $namespace"
+& $ocExe --token="$tokenContent" start-build $buildconfig --follow -n $namespace 2>&1 
+
 if (-not $?) {
                 Write-Error 'oc.exe failed. Exiting oc.ps1'
 				exit 100 
 }
 
-Write-Verbose "Leaving script oc.ps1"
+"Leaving script oc.ps1"
